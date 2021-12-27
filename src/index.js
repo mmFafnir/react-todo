ce283e1edc448ed1ecf108632a401c6aba4aaa8b
mmFafnir <akdav01@mail.ru>
2021-12-27 20:11:30 +0400

    route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {
+  BrowserRouter as Router,
+  Routes ,
+  Route,
+  Link,
+} from 'react-router-dom'
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
-    <App />,
+  <Router basename={process.env.PUBLIC_URL}>
+     <Routes >
+       <Route path='/' element={<App />}/>
+     </Routes>
+  </Router>,
   document.getElementById('root')
 );
